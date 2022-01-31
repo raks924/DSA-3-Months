60069eca038df60051a6c36d04dbfaf331ecc41c
raks924 <rakshitak256@gmail.com>
2022-02-01 00:50:40 +0530

    Reverse array

---
+++ Day3.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"enter the size of array";
+    cin>>n;
+
+    int arr[n];
+    cout<<"Enter your array"<<"\t";
+    for (int i = 0; i <= n; i++)
+    {
+        cin>>arr[i];
+    }
+
+
+    //Reverse array
+    // cout<<"The reverse array is as follows:";
+    // for (int i = n; i >= 0; i--)
+    // {
+    //     cout<<arr[i];
+    // }
+
+    // return 0;
+    
+    
+}
+
+
+    
