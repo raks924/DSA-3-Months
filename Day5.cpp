983ddfb65b48dd471785b785f6ec700f94ceffb5
raks924 <rakshitak256@gmail.com>
2022-02-06 01:44:34 +0530

    ARRAY

--- Day5.cpp
+++ Day5.cpp
@@ -2,5 +2,30 @@
 using namespace std;
 
 int main(){
+    int n,s;
+    cout<<"ente rthe size";
+    cin>>n;
+
+    int arr[n];
+    cout<<"enter the array";
+    for (int i = 0; i < n; i++)
+    {
+        cin>>arr[i];
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
     
+    //Xor Of CONSECUTIVE elements
+    int array[n];
+    for (int i = 0; i < n; i++)
+    {
+        array[i] = arr[i] ^ arr[i+1];
+    }
+
+    cout<<array[n];
+
+    return 0;    
 }
