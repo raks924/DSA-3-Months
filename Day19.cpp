94f11f5e8c409162c857e65fa4140cf75da7fa5f
raks924 <68598387+raks924@users.noreply.github.com>
2022-06-01 00:13:26 +0530

    31/05/22 Starting date

---
+++ Day19.cpp
@@ -0,0 +1,46 @@
+void MoveZeroesToEnd(int arr[]) {
+    for (int i = 0; i < 8; i++)
+    {
+        if (arr[i] == 0)
+        {
+            int j = i+1;
+            do
+            {
+                j++;
+            } while (arr[j] != 0);
+            swap(arr[i],arr[j]);
+            
+        }
+        
+    }
+    
+        
+}
+
+void Leader(int arr[],int n){
+    
+    for (int i = 0; i < n; i++)
+    {
+        bool flag = false;
+        for (int j = i+1; j < n; i++)
+        {
+            if (arr[i] <= arr[j])
+            {
+                flag = true;
+                break;
+            }
+            if (flag == false)
+            {
+                cout<<arr[i];
+            }
+            
+        }
+      
+    void ReverseAnArray(int arr[],int n){
+    for (int i = 0; i < (n-1)/2; i++)
+    {
+        swap(arr[i],arr[n-i-1]);
+        
+    }
+    
+}
