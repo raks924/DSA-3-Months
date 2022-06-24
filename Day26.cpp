05436f8afff91072835c2b419a375163055f2060
raks924 <68598387+raks924@users.noreply.github.com>
2022-06-25 00:10:27 +0530

    Update Day26.cpp

--- Day26.cpp
+++ Day26.cpp
@@ -100,3 +100,17 @@ string removeConsecutiveCharacter(string S)
         }
         return prefix;
     }
+
+void printDups(string str)
+{
+    map<char, int> count;
+    for (int i = 0; i < str.length(); i++) {
+        count[str[i]]++;
+    }
+ 
+    for (auto it : count) {
+        if (it.second > 1)
+            cout << it.first << ", count = " << it.second
+                 << "\n";
+    }
+}
