207f44e1098557a679f8e3f3f85718ee20d5a390
raks924 <68598387+raks924@users.noreply.github.com>
2022-06-24 14:10:29 +0530

    Palindrome,Anagram,ValidParentheses,RemoveConsecChar

--- Day26.cpp
+++ Day26.cpp
@@ -49,10 +49,36 @@ bool isAnagram(string s, string t) {
 
 
 
-    bool isValid(string s) {
-        for(int i = 0;i<s.length();i++){
-            if(s[i] == '(' || '{' || '[')
-                
-        }    
+   bool isValid(string s) {
+        int n = s.length();
+        stack<char> st;
+        for(int i=0;i<n;i++) {
+            if(s[i]=='(' || s[i]=='{' || s[i]=='[') {
+                st.push(s[i]);
+            } else if(s[i]==')' || s[i]=='}' || s[i]==']') {
+                if(st.empty()) return false;
+                if((s[i] == ')' && st.top()!='(') || 
+                   (s[i] == '}' && st.top()!='{') || 
+                   (s[i] == ']' && st.top()!='[')) 
+                    return false;
+                st.pop();
+            }
+        }
+        if(st.empty())
+            return true;
+        else
+            return false;
     }
-};
+
+string removeConsecutiveCharacter(string S)
+   {
+       int n = S.length();
+       string ans="";
+       int i=0;
+       while(i<n){
+           if(i+1==n || S[i]!=S[i+1])
+               ans+=S[i];
+           i++;
+       }
+       return ans;
+   }
