0f3a2fe1fd15855473bef016ff7080732c5ac99f
raks924 <68598387+raks924@users.noreply.github.com>
2022-08-12 01:51:46 +0530

    DupliArr,UniqueOccur

--- ADay4.cpp
+++ ADay4.cpp
@@ -15,3 +15,45 @@ sort(nums.begin(),nums.end());
     return ans;
 
     }
+
+
+
+
+
+
+  //sorting the array
+        sort(arr.begin(),arr.end());
+        
+        vector<int> ans;
+        int count=1;
+        
+        int i=0;
+        //iterating till less than arr.size()-1
+        while(i <arr.size()-1){
+            if(arr[i] == arr[i+1]){
+                count++;
+                i++;
+            }
+            else {
+                ans.push_back(count);
+                count = 1;
+                i++;
+            }
+        }
+        
+        
+        //pushing the last element of the array
+        ans.push_back(count);
+        
+        //sorting the vector which stores our number count
+        sort(ans.begin(),ans.end());
+        
+        //iterating through the Resultant vector to see if we have any duplicate count
+        i=0;
+        while(i<ans.size()-1){
+            if(ans[i] == ans[i+1]){
+                return false;
+            }
+            i++;
+        }
+        return true;
