387d53563dfb00a425ba2a592eefc0b1f53091d8
raks924 <68598387+raks924@users.noreply.github.com>
2022-08-13 01:58:39 +0530

    ctZeroes

--- ADay5.cpp
+++ ADay5.cpp
@@ -1,3 +1,11 @@
-
+int countZeroes(int arr[], int n) {
+        int ct =0;
+        for(int i = n-1;i>=0;i--){
+            if(arr[i] == 0 ){
+                ct++;
+            }
+        }
+        return ct;
+    }
   
   
