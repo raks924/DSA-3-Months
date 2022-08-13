e9e187314c72c5b4c91fe2acb0e72d4596430a76
raks924 <68598387+raks924@users.noreply.github.com>
2022-08-13 20:34:02 +0530

    Create ADay6.cpp

---
+++ ADay6.cpp
@@ -0,0 +1,40 @@
+vector<int>v1;
+   vector<int>v2;
+   for(int i = 0;i<n;i++){
+       if(arr[i]>=0){
+           v1.push_back(arr[i]);
+       }
+       else{
+           v2.push_back(arr[i]);
+       }
+   }
+   if(v1.empty()){
+       for(int i = 0;i<n;i++){
+           arr[i]=v2[i];
+       }
+   }
+   else if(v2.empty()){
+       for(int i = 0;i<n;i++){
+           arr[i]=v1[i];
+       }
+   }
+   else{
+   int i = 0;
+   int k = 0;
+   int j = 0;
+   while(i<n){
+       
+       if(k<v1.size()){
+       arr[i]=v1[k];
+       k++;
+       i++;
+       }
+       if(j<v2.size()){
+           
+           arr[i]=v2[j];
+           j++;
+           i++;
+           
+       }
+       }
+   }
