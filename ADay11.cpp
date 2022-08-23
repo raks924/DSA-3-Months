695d482977d2dde01bce4e0587bcd5565613464d
raks924 <68598387+raks924@users.noreply.github.com>
2022-08-24 01:45:35 +0530

    MissRepeatNo,MaxIndexDiff,BitonicPt,ArrProd,Maxprod

--- ADay11.cpp
+++ ADay11.cpp
@@ -35,3 +35,56 @@
 	    
 	    return arr[l];
 	}
+
+
+
+
+vector<int> productExceptSelf(vector<int>& nums) {
+       
+            vector<int> res;
+        int len=nums.size();
+        
+        int prefix[len], suffix[len];
+        int j=len-2, i=1;
+        
+        prefix[0]=suffix[j+1]=1;
+        
+        while(i<len){
+            prefix[i]=prefix[i-1]*nums[i-1];
+            suffix[j]=suffix[j+1]*nums[j+1];
+            
+            i++;
+            j--;
+        }
+        
+        for(i=0;i<len;i++)
+            res.push_back(prefix[i]*suffix[i]);
+        
+        return res;
+            
+           
+    }
+};
+
+
+
+int maxProduct(vector<int>& nums) {
+        int p = 1;
+        int mx = INT_MIN;
+        for(int i =0; i<nums.size(); i++){
+            p *= nums[i];
+            mx = max(mx, p);
+            if(p == 0)
+            p= 1;
+        }
+
+        p = 1;
+
+        for(int i =nums.size()-1; i>=0; i--){
+            p *= nums[i];
+            mx = max(mx, p);
+            if(p == 0)
+                p = 1;
+        }
+        return mx;
+    }
