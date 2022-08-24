447bb3f8d2535504404d27be95415ce459ef23cf
raks924 <68598387+raks924@users.noreply.github.com>
2022-08-24 10:35:52 +0530

    SearchRotatedSorted,Findmin

---
+++ ADay12.cpp
@@ -0,0 +1,72 @@
+int binarySearch(vector<int>& arr,int s,int e,int key){
+         int mid = s + (e-s)/2;
+        while(s<=e){        
+            
+            if(key == arr[mid])
+                return mid;
+            else if(key < arr[mid]){
+                return binarySearch(arr,s,mid-1,key);
+            }else{
+                return binarySearch(arr,mid+1,e,key);
+            }
+            mid = s + (e-s)/2;
+        }
+        return -1;   
+    }
+    
+    
+    int getPivot(vector<int>& nums, int n){
+        int s = 0 , e = n -1;
+        int mid = s + (e-s)/2;
+            
+        while(s<e){
+            
+            if(nums[mid] >= nums[0]){
+                s = mid+1;
+            }else{
+                e = mid;
+            }
+            mid = s + (e-s)/2;
+        }
+        return s;
+    }
+     
+    
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+        int pivot = getPivot(nums , n);
+        
+        if(target >= nums[pivot] && target <= nums[n-1]){
+            return binarySearch(nums,pivot,n-1,target);
+        }else{
+            return binarySearch(nums,0,pivot-1,target);
+        }
+    }
+
+
+
+
+
+int findMin(vector<int>& nums) {
+       int mn = INT_MAX;
+        int low = 0,high = nums.size()-1;
+        while(low<=high){
+            int mid = low + (high-low)/2;
+            if(nums[low]<=nums[mid]){
+                mn = min(mn,nums[low]);
+                low = mid+1;
+            }else{
+                mn = min(mn,nums[mid+1]);
+                high = mid;
+            }
+        }
+        return mn;
+    }
+
+
+
+
+
+
+
+
