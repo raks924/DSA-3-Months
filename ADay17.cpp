aab29ab43e9400ebfb060cd0c5cd82212425184e
raks924 <68598387+raks924@users.noreply.github.com>
2022-09-02 17:59:47 +0530

    LongestSubstrWithoutRepeat,LongRepCharReplace,GroupAnag

--- ADay17.cpp
+++ ADay17.cpp
@@ -21,3 +21,46 @@ int lengthOfLongestSubstring(string s) {
     }
 
 
+
+
+int characterReplacement(string s, int k) {
+        map<char,int>m;  // storing characters  and their count
+        int l =0,max_Occurance=0,res=0;
+        
+        for(int i =0 ;i < s.size() ; i++){
+            
+            m[s[i]]++;  // adding value to map
+            max_Occurance=max(max_Occurance,m[s[i]]);  // updating the maximum occuring element
+            
+            if(i-l+1-max_Occurance > k){  // if we get more operations in sliding window (invalid)
+                m[s[l]]--; // decreasing count from map
+                l++;  // increment  previous pointer
+            }
+            else res=max(res,i-l+1);    //update result
+        }        
+        return res;  
+    }
+
+
+
+
+
+vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        // Base case
+		if(strs.size() == 1)
+            return {{strs[0]}};
+        
+        vector<vector<string>> ans;
+        map<string, vector<string>> M;
+        for(int  i = 0; i < strs.size(); i++)
+        {
+            string str = strs[i];
+            sort(strs[i].begin(), strs[i].end()); // Sorting the string
+            M[strs[i]].push_back(str);  // Sorted string is the key and the value is the initial string
+        }
+        for(auto i = M.begin(); i != M.end(); i++)
+            ans.push_back(i -> second);  // Traversing the map and adding the vectors of string to ans
+        return ans;
+    }
+
+
