da728374702fe3540039c3f730acf8561228decc
raks924 <68598387+raks924@users.noreply.github.com>
2022-09-26 23:41:30 +0530

    RevWords,Anagram,Palindrome,replaceSpace,removeOccur

--- 22.cpp
+++ 22.cpp
@@ -59,3 +59,78 @@ bool isAnagram(string a, string b){
         return 1;
         
     }
+
+
+bool valid(char ch) {
+        if( (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') || (ch >= '0' && ch <= '9')) {
+            return 1;
+        }
+        
+        return 0;
+    }
+    
+    char toLowerCase(char ch) {
+    if( (ch >='a' && ch <='z') || (ch >='0' && ch <='9') )
+        return ch;
+    else{
+        char temp = ch - 'A' + 'a';
+        return temp;
+    }
+}
+    bool isPalindrome(string a) {
+    int s = 0;
+    int e = a.length()-1;
+
+    while(s<=e) {
+        if(a[s] != a[e])
+        {
+            return 0;       
+        }
+        else{
+            s++;
+            e--;
+        }
+    }
+    return 1;
+}
+bool checkPalindrome(string s)
+{
+     string temp = "";
+        
+        for(int j=0; j<s.length(); j++) {   
+            if(valid(s[j])) {
+                temp.push_back(s[j]);
+            }
+        }
+        
+        //lowercase me kardo
+        for(int j=0; j<temp.length(); j++) { 
+            temp[j] = toLowerCase(temp[j]);
+        }
+        
+        //check palindrome
+        return isPalindrome(temp);
+        
+
+    string replaceSpaces(string &str){
+	string temp = "";
+    
+    for(int i=0; i<str.length(); i++) {
+        if(str[i] == ' '){
+            temp.push_back('@');
+            temp.push_back('4');
+            temp.push_back('0');
+        }
+        else
+        {
+            temp.push_back(str[i]);
+        }
+    }
+    return temp;
+}
+    
+    
+    
+ while(s.length()!=0 && s.find(part) < s.length()) {
+            s.erase(s.find(part), part.length());
+        }
