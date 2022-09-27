0c6a4f9b14125cfc31f8e541cdf81ee66a657989
raks924 <68598387+raks924@users.noreply.github.com>
2022-09-27 23:48:33 +0530

    Stringpermute,ReplaceSpace,RemoveDupli,StrCompress

--- 23.cpp
+++ 23.cpp
@@ -78,3 +78,29 @@ string removeDuplicates(string s) {
         
         return ans;
     }
+
+
+int compress(vector<char>& chars) {
+    
+       int ansIndex = 0;
+        int n = chars.size();
+        int i = 0;
+        while(i < n){
+            int j = i+1;
+            while(j<n && chars[i] == chars[j]){
+                j++;
+            }
+            chars[ansIndex++] = chars[i];
+            
+            int count = j-i;
+            
+            if(count>1){
+            string ct = to_string(count);
+            for(char ch:ct){
+                chars[ansIndex++] = ch;
+            }
+            }
+            i = j;
+        }
+        return ansIndex;
+    }
